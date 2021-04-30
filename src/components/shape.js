9d93a42996bb5296435391d024f77f81fe8ed6ea
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-01 00:23:29 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+
+class Shape{
+
+     function drawShape(color){
+        return color;
+     }
+
+   function calculateArea(r){
+      return r*r;
+   }
+
+}
