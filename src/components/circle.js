ede2564584512b18b9c14ad62c4ef353bf816af9
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-01 00:37:43 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
-
+var Shape = require("./shape.js");
 // declare class
 
+class Circle extends Shape{
+     
+     function calculateArea(r){
+        return r*r;
+     }
+  
+}
 
 // export class using module.exports
